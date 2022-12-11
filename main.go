a93916e099d86643a481beb8af6060eeb0a5a3b2
paij0se <hachikopolarcito321@gmail.com>
2022-12-11 17:43:51 -0500

    a

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -14,33 +13,23 @@ import (
 )
 
 func goDotEnvVariable(key string) string {
-
-	// load .env file
 	err := godotenv.Load(".env")
-
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
-
 	return os.Getenv(key)
 }
 
 func TelegramBot() {
-
 	bot, err := tgbotapi.NewBotAPI(goDotEnvVariable("TELEGRAM_TOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates := bot.GetUpdatesChan(u)
-
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
@@ -54,8 +43,6 @@ func TelegramBot() {
 func main() {
 	// Echo instance
 	e := echo.New()
-	//e.Use(middleware.Logger())
-	//e.Use(middleware.Recover())
 	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Exotico")
@@ -63,19 +50,17 @@ func main() {
 	// Start server
 	go TelegramBot()
 	log.Println("http://localhost:8080")
-
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-// Handler
 func DiscordSend(username, text string) {
-	var jsonData = []byte(`{
+	var jsonData = []byte(`
+	{
 		"username": "` + username + `",
 		"avatar_url": "https://media.discordapp.net/attachments/1049891409878069248/1049914341589270538/2048px-Telegram_logo.png",
 		"content": "` + text + `"
 
 	}`)
-	fmt.Println(bytes.NewBuffer(jsonData))
 	req, err := http.NewRequest("POST", goDotEnvVariable("DISCORD_WEBHOOK"), bytes.NewBuffer(jsonData))
 	req.Header.Add("Content-Type", "application/json")
 	if err != nil {
@@ -88,6 +73,4 @@ func DiscordSend(username, text string) {
 		fmt.Println(err)
 	}
 	defer response.Body.Close()
-	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(body))
 }
